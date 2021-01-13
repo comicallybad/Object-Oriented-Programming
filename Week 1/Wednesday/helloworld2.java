122de4cbb4fbc0065e882acf6e87ba495036bce1
amp5646@yahoo.com <amp5646@yahoo.com>
2021-01-13 11:54:51 -0500

    Week 1 Wednesday

---
+++ Week 1/Wednesday/helloworld2.java
@@ -0,0 +1,9 @@
+public class helloworld2 {
+
+	public static void main(String[] args) {
+		// Main method begins execution of java application
+		System.out.print("Welcome to");
+		System.out.println("Java Programming!");
+	}//End method main
+
+}//End class helloworld2
---
+++ Week 1/Wednesday/helloworld4.java
@@ -0,0 +1,9 @@
+//Welcome 4 java application
+public class helloworld4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+	}
+
+}
