27e937aacbd5acf329e145ad0c443ab22bfc02de
amp5646@yahoo.com <amp5646@yahoo.com>
2021-02-19 17:43:54 -0500

    HW4

--- HW4/src/HW4Test.java
+++ HW4/src/HW4Test.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
 import java.io.File;  // Import the File class
-import java.io.IOException;  // Import the IOException class to handle errors
 
 //This is the test class that will run the code in the main function
 public class HW4Test {
